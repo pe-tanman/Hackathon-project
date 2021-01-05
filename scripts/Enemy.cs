1bf2663f0faa30bdfb87f87a89cbf407cab693d3
pe_tanman <pe.tanman.12@gmail.com>
2021-01-05 11:44:07 +0900

    最初

---
+++ scripts/Enemy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies
+{
+    string image_main;
+    string image_left;
+    public string name;
+    float maxHP;
+    float attack;
+    
+
+    public Enemies(string name, float maxHP, 
+    float attack, string image_main, string image_left)
+    {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.image_main = image_main;
+        this.image_left = image_left;
+        this.attack = attack;
+    }
+    
+     
+}
+public class Enemy : MonoBehaviour
+{
+    public GameObject prefab_tomato;
+    
+    Enemies tomato = new Enemies("tomato", 15, 1, 
+    "image/tomato.png", "image/tomato_left.png"); 
+    GameObject Tomato;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Tomato = Instantiate (prefab_tomato) as GameObject;
+    }
+    void Start()
+    {
+        
+    }
+    void move_Tomato()
+    {
+        Tomato.transform.Rotate(new Vector3(0,0,-150 * Time.deltaTime));
+        Vector3 pos = Tomato.transform.position;
+        Tomato.transform.position = new Vector3(pos.x + 3f * Time.deltaTime, pos.y, pos.z);
+    }
+    void Update()
+    {
+        move_Tomato();
+    }
+}
+
---
+++ scripts/learn.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test
+{
+    string name;
+    public Test(string name)
+    {
+        this.name = name;
+    }
+    public void avg(int math, int English)
+    {
+        Debug.Log(name + ":" + (math + English)/ 2);
+    }
+
+}
+public class learn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Test a01 = new Test("Sato");
+        a01.avg(50, 60);
+        Test a02 = new Test("Tanaka");
+        a02.avg(10, 90);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
