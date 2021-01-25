1058caf0a9d951494f566d90acd98f88d673aec1
pe_tanman <pe.tanman.12@gmail.com>
2021-01-25 21:34:15 +0900

    攻撃種類　追加

--- scripts/player.cs
+++ scripts/player.cs
@@ -8,6 +8,9 @@ public class player : MonoBehaviour
     Rigidbody2D rb;
     bool on_floor;
 
+    int weapon = 1;
+
+    int player_dir = 1;
 
     
     
@@ -17,33 +20,49 @@ public class player : MonoBehaviour
     {
         rb = this.GetComponent<Rigidbody2D>();
         GameObject tomato = this.gameObject;
+        Debug.Log(Tomato.hp);
     }
 
     // Update is called once per frame
     void Update()
     {
+        //移動
         if (on_floor){
             if(Input.GetKey(KeyCode.D))
             {
                 move_player(1);
+                player_dir = 1;
             }
             if(Input.GetKey(KeyCode.A))
             {
                 move_player(-1);
+                player_dir = -1;
             }
             if(Input.GetKeyDown(KeyCode.Space))
             {
                 rb.AddForce(new Vector2(0,300f));
-                Debug.Log("onspace");
             }
         }
+
+        //ゲームオーバー
         if (hp <= 0)
         {
             Debug.Log("gameover");
         }
+
+        //攻撃
         if (Input.GetMouseButtonDown(0))
         {
-            attack(100);
+            if (weapon == 1){
+                attack(1.5f, 1.5f);
+            }
+            if(weapon == 2){
+                attack(3, 1f);
+            }
+            if(weapon == 3){
+                attack(6, 0.5f);
+            }
+            
         }
     }
 
@@ -77,17 +96,17 @@ public class player : MonoBehaviour
                 rb.AddForce(new Vector2(800 *num * Time.deltaTime, 0)); 
             }
         }
-    void attack(float dis_ray)
+    void attack(float dis_ray, float damage)
     {
-        Ray2D ray = new　Ray2D(transform.position,Vector3.right); 
+        Ray2D ray = new　Ray2D(transform.position,new Vector2(player_dir, 0)); 
         RaycastHit2D hit = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction, dis_ray);
         
         if(hit.collider != null)
         {
             if(hit.collider.gameObject.tag == "enemy")
             {
-                Debug.Log(Tomato.hp);
-                Tomato.hp -= 1;
+                Debug.Log("Tomato.hp=" + Tomato.hp);
+                Tomato.hp -= damage;
             }
         }
         
